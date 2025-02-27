baaca912c18f5c017efa8bf34c2775255679f366
Douglas Maciel <douglasmacielaraujo@outlook.com>
2025-02-27 17:12:50 -0300

    Initial commit

---
+++ market-trends-analyzer/src/main/java/com/example/markettrends/MarketTrendsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.markettrends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTrendsAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketTrendsAnalyzerApplication.class, args);
+	}
+
+}
